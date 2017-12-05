3c643dea4abd7248ce3c896c582c730c7feb244f
DJ <33149171+PlaguedByWeirdness@users.noreply.github.com>
2017-12-05 21:13:06 +0100

    Making nice comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,40 @@
+/*                   
+                                  /   \       
+ _                        )      ((   ))     (
+(@)                      /|\      ))_((     /|\
+|-|                     / | \    (/\|/\)   / | \                      (@)
+| | -------------------/--|-voV---\`|'/--Vov-|--\---------------------|-|
+|-|                         '^`   (o o)  '^`                          | |
+| |                               `\Y/'                               |-|
+|-|                              Authors                              | |
+| |                            Szilard Gal                            |-|
+|-|                   Vujovic Jelena & Mrdalj Denis                   | |
+| |                                                                   |-|
+|_|___________________________________________________________________| |
+(@)              l   /\ /         ( (       \ /\   l                `\|-|
+                 l /   V           \ \       V   \ l                  (@)
+                 l/                _) )_          \I
+                                   `\ /'
+                                     '                        
+ */
+
+
 #include <Arduino.h>
-#include "../lib/LiquidCrystal_I2C/LiquidCrystal_I2C.h"
-#include "characters.h"
+#include "../lib/LiquidCrystal_I2C/LiquidCrystal_I2C.h" // Library for LCD
+#include "characters.h"					// Library that includes segments for numbers/letters
 
 #define Serial_Baud 9600
 
-#define LCD_ADDRESS_SCORE 0x3E
-#define LCD_ADDRESS_DEBUG 0x3F
+#define LCD_ADDRESS_SCORE 0x3E		// LCD for displaying score
+#define LCD_ADDRESS_DEBUG 0x3F		// LCD for displaying current position
 
-char buffer[8];
+char buffer[8];				// Defining buffer array which will hold all the data about the tool we are using
 bool buffer_read = false;
 
 char tool_id; int item_id; char function_id; unsigned char value1; unsigned char value2; unsigned char value3; unsigned char value4; unsigned char value5;
 
-LiquidCrystal_I2C lcd(LCD_ADDRESS_SCORE,16,2);
-LiquidCrystal_I2C lcd_debug(LCD_ADDRESS_DEBUG,16,2);
+LiquidCrystal_I2C lcd(LCD_ADDRESS_SCORE,16,2);		// Setting score LCD
+LiquidCrystal_I2C lcd_debug(LCD_ADDRESS_DEBUG,16,2);	// Setting debug LCD
 
 void custom0(int col)
 { // Uses segments to build numbers/letters, in this case numbers from 0 to 9
@@ -162,11 +183,12 @@ void printNumber(int value, int col) {
 
 void display_number(int first, int second, int third) {
   printNumber(first, 7);		  //Printing digit x, starting on col 7
-  printNumber(second, 10);		//Printing digit y, starting on col 10
-  printNumber(third, 13);     //Printing digit z, starting on col 13
+  printNumber(second, 10);		  //Printing digit y, starting on col 10
+  printNumber(third, 13);    		  //Printing digit z, starting on col 13
 }
 
 void setup_dispaly() {
+  //Creating characters for numbers
   lcd.createChar(1,bar1);
   lcd.createChar(2,bar2);
   lcd.createChar(3,bar3);
@@ -194,14 +216,17 @@ void setup_dispaly() {
   lcd.setCursor(3,1);
   lcd.write(2);
 
+  // First we display this
   display_number(0,0,0);
 }
 
 
 void setup() {
   Serial.begin(Serial_Baud);
+  // Inicializing score display
   lcd.init();
   lcd.backlight();
+  // Inicializing debug display
   lcd_debug.init();
   lcd_debug.backlight();
 
@@ -210,22 +235,24 @@ void setup() {
 }
 
 void loop() {
+    // Reading data from second byte
     if(Serial.available() >= 8) {
+      // All the data goes to buffer
       Serial.readBytes(buffer, 8);
       buffer_read = true;
     }
 
     if(buffer_read) {
-      tool_id     = buffer[0];
-      item_id     = buffer[1];
-      function_id = buffer[2];
-      value1      = buffer[3];
-      value2      = buffer[4];
-      value3      = buffer[5];
-      value4      = buffer[6];
-      value5      = buffer[7];
+      tool_id     = buffer[0];		// It can be 'd' or 'b'
+      item_id     = buffer[1];		// It can be 'd' or 's'
+      function_id = buffer[2];		// It can be 'f', 'u' or 'c'
+      value1      = buffer[3];		// Score mode: first number	Debug mode: First byte of x-axis value
+      value2      = buffer[4];		// Score mode: second number	Debug mode: Second byte of x-axis value
+      value3      = buffer[5];		// Score mode: third number	Debug mode: First byte of y-axis value
+      value4      = buffer[6];		// 				Debug mode: Second byte of y-axis value
+      value5      = buffer[7];		// Useless shit
 
-      if(tool_id == 'd') {
+      if(tool_id == 'd') { // display
         if(item_id == 's') {  // score
           if(function_id == 'u') { // update
             display_number(value1,  value2, value3);
@@ -242,10 +269,10 @@ void loop() {
             lcd_debug.clear();
             lcd_debug.setCursor(0, 0);
             lcd_debug.print("X: ");
-            lcd_debug.print((value1 << 8) + value2);
+            lcd_debug.print((value1 << 8) + value2);	// Since we need 16-bit for whole value (its bigger than 256) we need to use 2 seperate values and merge them together by shifting value1 to left for 8bits and adding value2
             lcd_debug.setCursor(0, 1);
             lcd_debug.print("Y: ");
-            lcd_debug.print((value3 << 8) + value4);
+            lcd_debug.print((value3 << 8) + value4);	// Same as for x-axis except we are using value3 and value4
 
           } else if(function_id == 'i') { // info
             // display info like: what side, battery and info about startup
@@ -279,12 +306,12 @@ void loop() {
 
           }
         }
-      } else if(tool_id == 'b') { // board
+      } else if(tool_id == 'b') { // board (useless shit)
 
       }
 
       Serial.write(1); // signaling glavna that everything is okay
 
-      buffer_read = false;
+      buffer_read = false;	//Turn it off after every iteration so we wait for next glavna signal
     }
 }
